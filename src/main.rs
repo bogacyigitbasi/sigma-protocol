5eaf908122d395b91e4cbd5a302cedc300b35651
Boğaçhan Yiğitbaşı <by@concordium.com>
2024-08-10 12:22:21 +0300

    added better comments and variable names for clarity

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@
 use rand::Rng;
 
 // Lets recap the scenario prover knows a value is where g^x = y mod p
-
-// 1) Prover picks a random value k, calculates value commitment r = g^k mod p and sends it to verifier
-// 2) Verifier picks another random value v, calculates challenge c = g^v mod p
-// 3) Prover calculates the response z = r + x*c mod p and sends it to verifier
-// 4) Verifier will compare if g^z = y^c*z
+// 1) Prover picks a random value r, calculates value commitment com = g^r mod p and sends it to verifier
+// 2) Verifier picks another random value c as challenge, send it to prover
+// 3) Prover calculates the response resp = r + x*c mod p-1 and sends it to verifier
+// 4) Verifier will compare if g^resp mod p == com*y^c mod p
 
 fn modular_exp(g: u64, x: u64, p: u64) -> u64 {
     let mut result = 1;
@@ -35,28 +34,28 @@ fn main() {
     // prime modular value to work in finite field
     const p: u64 = 101;
     // witness value -- we want to keep this secret where g^x = y mod p
-    let x = 9;
+    let witness = 9;
     // calculate y
-    let y = modular_exp(g, x, p);
+    let y = modular_exp(g, witness, p);
 
     // now lets prove that we know the value 9 without sharing it with verifier
-    // 1 commitment => r = g^k mod p
+    // 1 commitment => a = g^r mod p
     // picked random from 1 to p and sent it to modular exponential function to calculate the commitment
     let r = rng.gen_range(1..p);
-    let a = modular_exp(g, r, p);
-    println!("Prover's commitment : r = {}", a);
+    let com = modular_exp(g, r, p);
+    println!("Prover's commitment : r = {}", com);
     // 2 generate a challenge to use it to generate the response.
     // assume this is done at verifier side
-    let c: u64 = rng.gen_range(1..p);
-    println!("Verifier's challenge : c = {}", c);
+    let challenge: u64 = rng.gen_range(1..p);
+    println!("Verifier's challenge : c = {}", challenge);
     // 3 calculate the response z = r + x*c mod p -1
-    let z = (r + c * x) % (p - 1);
-    println!("Prover's response: z = {}", z);
+    let resp = (r + challenge * witness) % (p - 1);
+    println!("Prover's response: z = {}", resp);
 
-    // final step verifier cchecks g^z mod p == a * y^z mod p
-    if (modular_exp(g, z, p) == (a * modular_exp(y, c, p)) % p) {
-        println!("Prover's response z {} is valid!", z);
+    // final step verifier cchecks g^z mod p == a * y^c mod p
+    if (modular_exp(g, resp, p) == (com * modular_exp(y, challenge, p)) % p) {
+        println!("Prover's response z {} is valid!", resp);
     } else {
-        println!("Prover's response z {} is invalid!", z);
+        println!("Prover's response z {} is invalid!", resp);
     }
 }
