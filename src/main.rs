7281372d70ff872825e7dea3487126d517e940c9
Boğaçhan Yiğitbaşı <by@concordium.com>
2024-09-10 10:06:16 +0300

    verifier

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // we will use RistrettoPoint to simplify implementation and prevent cofactor issue.
 // which explained quite well here: https://crypto.stackexchange.com/a/56345
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT; // base or generator g
 use curve25519_dalek::ristretto::RistrettoPoint; // a point on Ristretto group to prevent cofactor issue
 use curve25519_dalek::scalar::Scalar; // witness, commitment, or challenge will be a scalar value. also the private key
+use curve25519_dalek::{constants::RISTRETTO_BASEPOINT_POINT, scalar}; // base or generator g
 use rand::{rngs::OsRng, Rng}; // random
 use sha2::{Digest, Sha256};
 
@@ -15,7 +15,7 @@ struct Prover {
 
 impl Prover {
     //constructor of Prover
-    fn new(&self, privKey: Scalar) -> Self {
+    fn new(&mut self, privKey: Scalar) -> Self {
         let pubKey = &self.privKey * &RISTRETTO_BASEPOINT_POINT; // G is selected as RISTRETTO_BASEPOINT_POINT
         Prover {
             privKey: privKey,
@@ -32,11 +32,26 @@ impl Prover {
         (commitment, r)
     }
 
-    // compute response using the commitment, challenge and witness
+    // compute response using the commitment, challenge and witness (privKey)
     // resp = r + x*C
-    fn proof(&self, challenge: RistrettoPoint, r: Scalar) -> Scalar {
+    fn proof(&self, challenge: Scalar, r: Scalar) -> Scalar {
         r + &self.privKey * challenge
     }
 }
 
+struct Verifier {
+    challenge: Scalar,
+}
+
+impl Verifier {
+    fn generate_challenge(&mut self, rng: &mut OsRng) -> Scalar {
+        let r = rng.gen();
+        self.challenge = r;
+        self.challenge
+    }
+    // g^z == a * y^z
+    fn verify(&self, commitment: RistrettoPoint, pubKey: RistrettoPoint, proof: Scalar) -> bool {
+        RISTRETTO_BASEPOINT_POINT * proof == commitment + (self.challenge * pubKey)
+    }
+}
 fn main() {}
